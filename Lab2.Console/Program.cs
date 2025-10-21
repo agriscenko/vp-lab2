c21c5dfc2e3593d3b4b93b76969405b355c6f0a7
agriscenko <andrey.griscenko@gmail.com>
2025-10-21 22:23:38 +0300

    Add an example using FirstOrDefault(); fix formatting.

--- Lab2.Console/Program.cs
+++ Lab2.Console/Program.cs
@@ -14,18 +14,18 @@ internal class Program
 
         Linq1(db);
         Linq2(db);
+        Linq3(db);
     }
 
     [Description("Izvadīt nodaļu skaitu pirmajā stāvā.")]
     private static void Linq1(DepartmentDbContext db)
     {
-        var results = db.Departments
-            .Where(d => d.FloorNumber == 1);
+        var results = db.Departments.Where(d => d.FloorNumber == 1);
 
-        System.Console.WriteLine($"Nodaļu skaits pirmajā stāvā: {results.Count()}");
+        System.Console.WriteLine($"Nodaļu skaits pirmajā stāvā: {results.Count()}.");
     }
 
-    [Description("Izvadīt visu trešā stāva nodaļu nosaukumus un aprakstus, kā arī katras nodaļas darbiniekus un viņu amatus.")]
+    [Description("Izvadīt visus trešā stāva nodaļu nosaukumus un aprakstus, kā arī katras nodaļas darbiniekus un viņu amatus.")]
     private static void Linq2(DepartmentDbContext db)
     {
         var results = db.Departments
@@ -40,6 +40,21 @@ internal class Program
             {
                 System.Console.WriteLine($"Darbinieks: {employee.FirstName} {employee.LastName}, Amats: {employee.Position}");
             }
+        }
+    }
+
+    [Description("Izvadīt nodaļas nosaukumu ar ID=100, izmantojot FirstOrDefault().")]
+    private static void Linq3(DepartmentDbContext db)
+    {
+        var department = db.Departments.FirstOrDefault(d => d.Id == 100);
+
+        if (department != null)
+        {
+            System.Console.WriteLine($"Atrasta nodaļa: {department.Name}");
+        }
+        else
+        {
+            System.Console.WriteLine("Neviena nodaļa ar ID=100 netika atrasta!");
         }
     }
 }
